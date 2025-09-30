798ba9fff14e866380baf04e62038d72170fa01b
t-young31 <tom.young.316@gmail.com>
2025-09-30 17:53:52 +0100

    add graceful serve func

---
+++ pkg/graceful/serve_test.go
@@ -0,0 +1,48 @@
+package graceful
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServeLogStream(t *testing.T) {
+	logBuffer := &bytes.Buffer{}
+	log.Logger = zerolog.New(logBuffer)
+
+	go Serve(&http.Server{Handler: http.NewServeMux()}, 10*time.Millisecond)
+	time.Sleep(100 * time.Millisecond) // some startup time
+
+	process := must(os.FindProcess(os.Getpid()))
+	err := process.Signal(syscall.SIGINT)
+	assert.NoError(t, err)
+	time.Sleep(20 * time.Millisecond) // some shutdown time > shutdown duration
+
+	logStream := string(must(io.ReadAll(logBuffer)))
+	expectedLines := []string{
+		"Started HTTP server",
+		"Recieved termination signal",
+		"Closing server",
+		"Server exited",
+	}
+	for _, expectedLine := range expectedLines {
+		assert.Contains(t, logStream, expectedLine)
+	}
+	assert.Regexp(t, regexp.MustCompile(`Started.*\n.*Recieved termination signal`), logStream)
+}
+
+func must[T any](obj T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return obj
+}
